91a016edd25260f237036691208ce08360becb37
Jesusmar Lima <jesusmarlima@Jesusmars-MacBook-Pro.local>
2015-10-02 16:21:47 -0400

    Adding validations and parcials to contact

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,4 @@
 class Contact < ActiveRecord::Base
   belongs_to :item
+  validates :name, :email, :message , presence: true
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ActiveRecord::Base
   belongs_to :category
   has_many :contacts
+  validates :name, presence: true
 end
